bdfaa88b9e0748bfe2039e8fb9286e6daf43741d
Randy MS <randyma01@gmail.com>
2023-09-10 14:10:14 -0600

    sort files

--- c/factorial.c
+++ c/factorial.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 unsigned long long factorial(long);
 
@@ -8,10 +9,21 @@ int main(int argc, char *argv[])
 
     unsigned long long facto;
     long num = strtol(argv[1], NULL, 10);
+    clock_t time;
 
+    time = clock();
     facto = factorial(num);
+    time = clock() - time;
 
+    double time_taken = ((double)time) / CLOCKS_PER_SEC;
+
+    printf("\n");
+    printf("-------------------------------------\n");
     printf("Factorial of %s is: %llu\n", argv[1], facto);
+    printf("\n");
+    printf("Elapse time: %f\n", time_taken);
+    printf("-------------------------------------");
+    printf("\n");
 
     return 0;
 }
